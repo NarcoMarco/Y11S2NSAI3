955153d9af8d64bc684c1b2c70c2e5964c6bcc07
NarcoMarco <marcocondipodero@gmail.com>
2024-10-30 14:46:42 +1100

    started on v2

--- v2/Topologies/scripts/attack-box.sh
+++ v2/Topologies/scripts/attack-box.sh
@@ -3,8 +3,10 @@
 # Update the system
 apk update
 
+# Install packages
+apk add iproute2 openssh openrc bash termshark nmap
+
 # Configure network
-apk add iproute2
 ip addr add 192.168.0.47/24 dev eth1
 ip link set eth1 up
 
@@ -13,13 +15,25 @@ ip route add default via 192.168.0.1 dev eth1 metric 100
 ip route add default via 172.20.20.1 dev eth0 metric 200
 
 # Add hackerman user
-adduser -s hackerman
+adduser -D hackerman 
+echo 'hackerman:hackerman' | chpasswd
+
+# Add hackerman to wireshark group
+adduser hackerman wireshark
 
 # Configure ssh
-apk add openssh openrc bash
+# Ensure OpenRC directories exist
+mkdir -p /run/openrc
+touch /run/openrc/softlevel
 
+# Start and enable rsyslog
+rc-service rsyslog start
+rc-update add rsyslog
+
+sh /config/services.sshd.sh 22
+rc-status -a
+
+rc-service sshd start
 rc-update add sshd
 
-service sshd start
-
-exit
+rc-status -a
--- v2/Topologies/scripts/r1.sh
+++ v2/Topologies/scripts/r1.sh
@@ -1,17 +1,20 @@
+#!/bin/sh 
+
+echo "setting up router"
+
+vtysh << EOF
 configure terminal
 
-# Configure server port
 interface eth1
 ip address 192.168.143.1/24
 
-# Configure admin switch
 interface eth2
 ip address 10.13.37.1/24
 
-# Configure client switch
 interface eth3
 ip address 192.168.0.1/24
 
 end
 write memory
 exit
+EOF
