bd78291d95463a81d3701cfc7342d65899ce650c
NarcoMarco <marcocondipodero@gmail.com>
2024-11-02 17:34:10 +1100

    fixed qc-client2.sh

--- v4/Topologies/scripts/qc-client2.sh
+++ v4/Topologies/scripts/qc-client2.sh
@@ -7,11 +7,11 @@ apk update
 apk add iproute2 busybox-extras
 
 # Configure Network
-ip addr add 192.168.0.18/24 dev eth1
+ip addr add 192.168.1.18/24 dev eth1
 ip link set eth1 up
 
 ip route del default via 172.20.20.1 dev eth0
-ip route add default via 192.168.0.1 dev eth1 metric 100
+ip route add default via 192.168.1.1 dev eth1 metric 100
 ip route add default via 172.20.20.1 dev eth0 metric 200
 
 # Run script to talk to quickchat server
